fca5c50e4908948fd85a1e202e9b35cd854708e1
Kev <kevinxpt27@example.com>
2024-08-10 22:42:53 -0500

    remove files

--- dist/components/sb-item-body.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef, Renderer } from '@angular/core';
-export declare class SBItemBody {
-    private renderer;
-    bodyEl: ElementRef;
-    constructor(renderer: Renderer);
-    ngAfterViewInit(): void;
-    toggle(collapsed: boolean): void;
-    setHeight(height: string): void;
-}
--- dist/components/sb-item-head.d.ts
+++
@@ -1,6 +0,0 @@
-import { SBItem } from './sb-item';
-export declare class SBItemHead {
-    private sbItem;
-    constructor(sbItem: SBItem);
-    toggleClick(event: Event): void;
-}
